28938b2ecefa788ee9e6f03cb3c3262604b450f5
HP\user <aulakovs@gmail.com>
2022-09-21 14:45:56 +0300

    Animation added

--- Jquery.js
+++ Jquery.js
@@ -1,3 +1,23 @@
+$( function() {
+    var state = true;
+    $( "#button" ).on( "click", function() {
+      if ( state ) {
+        $( "#effect" ).animate({
+          backgroundColor: "#0000FF",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+    });
+});
+
 $(document).ready(function(){
     $("p").hover(function(){
         $(this).addClass("highlight");
@@ -16,3 +36,4 @@ $(document).ready(function(){
         $("ul").slideDown();
     });
 });
+
